04dcca14b19678b232b289defb0f316e9e435c28
PashaSeminls <86929042+PashaSeminls@users.noreply.github.com>
2022-05-12 13:50:15 +0300

    Объявления класса и его полей

---
+++ Src/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace PM03
+{
+    //Обьявления класса и его полей
+    public class Auto : IComparable
+    {
+        public string markaauto;
+        public string modelauto;
+        public string cenaauto;
+
+        public Auto(string marka, string model, string cena)
+        {
+            this.markaauto = marka;
+            this.modelauto = model;
+            this.cenaauto = cena;
+        }
+
+        public string ToString()
+        {
+            return string.Format("Марка автомобиля: {0}  Модель автомобиля: {1} Цена автомобиля: {2}", markaauto, modelauto, cenaauto);
+        }
+
+  
+    
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Количество автомобилей:");
+            int cntAutos = Convert.ToInt32(Console.ReadLine());
+            Driver drivers = new Driver(cntAutos);
+            drivers.Fill();
+            drivers.Sort();
+            drivers.PrintToFile();
+            Console.WriteLine("Результат приложения выведен в файл Resultatprogrammi");
+        }
+    }
+}
+
+
